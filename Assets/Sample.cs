6cd82c72ad2b4514c0ab0534c86943dd193823cc
catsnipe <catsnipe@gmail.com>
2021-10-26 23:54:36 +0900

    create new

---
+++ Assets/Sample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    [SerializeField]
+    SimpleUIEase[]    transitions;
+
+    void Awake()
+    {
+        // 開始時に全部消す
+        foreach (var trans in transitions)
+        {
+            trans.SetValue(0);
+        }
+    }
+
+    public void OnClickShow()
+    {
+        foreach (var trans in transitions)
+        {
+            trans.Show();
+        }
+    }
+
+    public void OnClickHide()
+    {
+        foreach (var trans in transitions)
+        {
+            trans.Hide();
+        }
+    }
+}
